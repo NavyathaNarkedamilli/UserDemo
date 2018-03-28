2b97752a33da91f18261a28fc59cc119e863747c
uf62 <=navyatha.narkedamilli@in.tesco.com>
2018-03-28 10:59:51 +0530

    background image changes

--- UI/src/Home.js
+++ UI/src/Home.js
@@ -30,18 +30,18 @@ export default class Home extends Component {
     }
     render() {
         return (
-            <div className="container background">
+            <div className="container">
                 <div className="row logindiv">
                     {this.state.message && <div className="row">
-                        <div className="col-md-8">  <h3 className="header">Welcome to HomePage</h3></div></div>}
+                        <div className="col-md-8">  <h3 className="loginheader">Welcome to HomePage</h3></div></div>}
                     {!this.state.enableRegisterComponent && !this.state.enableLoginComponent ?
                         <div className="row">
                             <div>
-                                <div className="col-md-2">
+                                <div className="col-md-4">
                                     <input type="submit" value="Click here to Login" className="btn btn-primary btn-circle" onClick={this.renderLogin.bind(this)}></input>
                                 </div>
                             </div>
-                            <div className="col-md-2"><input type="submit" value="Click here to Register" className="btn btn-primary btn-circle" onClick={this.renderRegister.bind(this)}></input>
+                            <div className="col-md-4"><input type="submit" value="Click here to Register" className="btn btn-primary btn-circle" onClick={this.renderRegister.bind(this)}></input>
                             </div>
                         </div> : ""}
 
